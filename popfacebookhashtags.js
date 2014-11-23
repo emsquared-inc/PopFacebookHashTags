9152a42169e810141e92f6501ceb00cdc41439c0
emsquared-inc <eric.mintz@emsquared-inc.com>
2014-11-23 10:00:55 -0500

    Create popfacebookhashtags.js
    Initial entry

---
+++ popfacebookhashtags.js
@@ -0,0 +1,46 @@
+// ==UserScript==
+// @name         Pop Facebook Hash Tags
+// @namespace    http://www.emsquared-inc.com/
+// @version      1.0 alpha
+// @description  Makes hashtag links open in a new window or tab
+// @author       Eric Mintz
+// @match        https://www.facebook.com/*
+// @match        https://www.facebook.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+
+	// Manage handlers
+	addEventListeners = function() {
+		document.addEventListener ('load',fixFacebookLinks, false );
+		document.addEventListener ('DOMNodeInserted',fixFacebookLinks, false);
+    }
+	removeEventListeners = function() {
+	    document.removeEventListener('load',fixFacebookLinks);
+	    document.removeEventListener('DOMNodeInserted',fixFacebookLinks);
+	}
+
+	// Modify hashtag links
+	var fixFacebookLinks = function() {
+
+	    // remove handlers to prevent recursion
+	    removeEventListeners();
+    
+	    // find hashtag links
+    	var allLinks = document.getElementsByTagName('a');
+		for (var i = 0; i < allLinks.length; i++) {
+    		if (allLinks[i].href.contains('facebook.com/hashtag/')) {
+            
+        	    // make the linke pop open a new brower window/tab
+       			allLinks[i].setAttribute('target','_blank');
+	    	}
+		}
+    
+	    // restore the handlers
+	    addEventListeners();
+	};
+
+	// Add listeners
+	addEventListeners();
+})();
